d4cab78d1166da63a2df7f836f8bf60df5523871
Joel Hainley <joel@jwbsolutions.com>
2022-05-11 07:58:40 -0700

    initial checkin

---
+++ .emacs.d/elpa/zeno-theme-20211205.2148/zeno-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from zeno-theme.el  -*- no-byte-compile: t -*-
+(define-package "zeno-theme" "20211205.2148" "A dark theme using different shades of blue" '((emacs "24")) :commit "70fa7b7442f24ea25eab538b5a22da690745fef5" :authors '(("Bharat Joshi" . "jbharat@outlook.com")) :maintainer '("Bharat Joshi" . "jbharat@outlook.com") :keywords '("faces" "theme" "dark" "blue") :url "https://github.com/jbharat/zeno-theme")
